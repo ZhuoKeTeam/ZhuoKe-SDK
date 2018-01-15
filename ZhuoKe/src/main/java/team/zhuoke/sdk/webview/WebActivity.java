47998af973ae2f0a0d366ddaaa5e0e62f3d44b62
lvwenbo0107 <443818654@qq.com>
2018-01-15 21:50:30 +0800

    新建webview

---
+++ ZhuoKe/src/main/java/team/zhuoke/sdk/webview/WebActivity.java
@@ -0,0 +1,72 @@
+package team.zhuoke.sdk.webview;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import team.zhuoke.sdk.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView webView;
+    WebSettings mWebSettings;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+    protected void initData() {
+        mWebSettings= webView.getSettings();
+        //设置自适应屏幕，两者合用
+        mWebSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        mWebSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+        //缩放操作
+        mWebSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        mWebSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        mWebSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        //设置字体大小
+        Resources res = getResources();
+        int fontSize = (int) res.getDimension(R.dimen.txtSize);
+        mWebSettings.setDefaultFontSize(fontSize);
+        //其他细节操作
+        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        mWebSettings.setAllowFileAccess(true); //设置可以访问文件
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        mWebSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        mWebSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setAppCacheMaxSize(1024 * 1024 * 8);
+        String appCachePath = webView.getContext().getCacheDir().getAbsolutePath();
+        mWebSettings.setAppCachePath(appCachePath);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setAppCacheEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+//                NBSWebChromeClient.initJSMonitor(view, newProgress);
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mWebSettings.setJavaScriptEnabled(false);
+
+    }
+}
